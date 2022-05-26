1218531556fb1ec368978c98ac49bc523d5f6d6a
REDEYEDEVIL <98946805+REDEYEDEVIL@users.noreply.github.com>
2022-05-26 20:40:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 burger=document.querySelector('.burger')
 navbar=document.querySelector('.navbar')
 navlist=document.querySelector('.navlist')
+snav=document.querySelector('.snav')
+
 
 burger.addEventListener('click', ()=>{
-    navbar.classList.toggle('h-class');
+    navbar.classList.toggle('h-nav');
+    snav.classList.toggle('v-class');
     navlist.classList.toggle('v-class');
 })
